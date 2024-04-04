61360bf484f2fd425503ae04368830adb38fee1d
Tasha290929 <138973005+Tasha290929@users.noreply.github.com>
2024-04-04 14:14:01 +0300

    first commit

---
+++ data/blog.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 04, 2024 at 02:06 PM
+-- Server version: 8.0.30
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique` (`email`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Лабораторная работа №9
+## Задание 1. Выберите базу данных, с которой вы планируете работать:
+
+Для выполнения лабораторной работы я выбрала `mysql (mariadb)` ,так как она подходит для проектов среднего и крупного масштаба,
+где необходима поддержка высокой производительности и
+масштабируемости. MariaDB, форк MySQL (то есть основана на MySQL),
+также предоставляет дополнительные функции и улучшенную
+производительность.
+
+## Задание 2. Создайте базу данных blog и создайте две базы данных users и comments с помощью языка DDL (CREATE TABLE).
+
+Структура баз данных:
+
+* users:
+
+![](./Screenshot%202024-04-04%20135302.png)
+
+```
+CREATE TABLE IF NOT EXISTS User
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    email   VARCHAR(255) NOT NULL
+);
+```
+
+* comments:
+
+![](./Screenshot%202024-04-04%20135335.png)
+
+```
+CREATE TABLE IF NOT EXISTS Comments
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    comment TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
+```
+
+## Экспортируем базу данных `blog.sql` в папку `/data`
+
+## Примечание 
+
+Для выполения данной лабораторной работы я использовала OpenServer. Эта база данных предполагает хренение данных пользователей и их коммернтариев. 
